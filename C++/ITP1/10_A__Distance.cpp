e43cf4cf81979aefc21ed9b4bd5502af89f28ba4
eyamagishi <etsuro.yamagishi0625@gmail.com>
2023-04-13 22:44:58 +0900

    add: 10__Aの実装

--- C++/ITP1/10_A__Distance.cpp
+++ C++/ITP1/10_A__Distance.cpp
@@ -1,7 +1,20 @@
 #include<iostream>
+#include<iomanip>
+#include<cmath>
 using namespace std;
 
 int main(){
-
+  double x1, y1, x2, y2;
+  cout << fixed << setprecision(4);
+  cin >> x1 >> y1 >> x2 >> y2;
+  cout << sqrt( pow(x2 - x1, 2) + pow(y2 - y1, 2) ) << endl;
   return 0;
 }
+
+// ２点 P1(x1, y1), P2(x2, y2) の距離を求めるプログラムを作成せよ。
+
+// Input
+// x1, y1, x2, y2 （実数）が空白区切りで与えられます。
+
+// Output
+// P1とP2の距離を実数で１行に出力して下さい。0.0001以下の誤差があってもよいものとします。
